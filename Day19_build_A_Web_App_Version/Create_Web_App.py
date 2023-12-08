a79d9296820317532d87725bac6fee90ceaee547
kiran <kiranjojan100@gmail.com>
2023-12-08 23:08:32 +0530

    Creating Web App checkbox and todo list's

--- Day19_build_A_Web_App_Version/Create_Web_App.py
+++ Day19_build_A_Web_App_Version/Create_Web_App.py
@@ -1,13 +1,20 @@
 import streamlit as st
 import functions
 
-result_read = functions.get_read_lines()
+
+result = functions.get_read_lines()
+
+def add_todo():
+    item = st.session_state["new_todo"]   # call the key to store new todos items
+    result.append(item)
+    functions.get_write_lines(result)
 
 st.title("My Todo App")
 st.subheader("This is SubHead of Todo App ")
 st.write("This is a simple text")
 
-for item in result_read :
+for item in result :
     st.checkbox(item)
 
-st.text_input(label="", placeholder="Add New Todo's" )
+st.text_input(label="", placeholder="Add New Todo's",
+               on_change=add_todo, key='new_todo')           #on_change return custom key word
